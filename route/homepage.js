5dfe812f038b0543b00bed3e0b998f331e56afa2
Md Samiul <samiuljust2018@gmail.com>
2022-01-28 00:48:20 +0600

    update

--- route/homepage.js
+++ route/homepage.js
@@ -2,17 +2,30 @@ const db=require('../model/database')
 const Product=db.products
 
 exports.postData=async(req,res,next)=>{
-    //console.log(req.body.product_name[0])
+    //console.log(req.body)
 
     try{
 
-        let product1=new Product({
-            product_name:req.body.product_name[0],
-            product_price:req.body.product_price[0],
-            product_qnt:req.body.product_qnt[0]
-        })
 
-        console.log(product1)
+        
+          let array=[];
+          let current_product={};
+
+            for(let i=0;i<req.body.product_name.length;i++){
+                current_product['product_name']=req.body.product_name[i];
+                current_product['product_price']=req.body.product_price[i];
+                current_product['product_qnt']=req.body.product_qnt[i];
+                array.push(current_product)
+            }
+            //console.log(array)
+
+            let result=await Product.bulkCreate(array);
+            if(result){
+                console.log(result);
+            }else{
+                console.log('error')
+            }
+
 
     }catch(e){
         console.log(e)
